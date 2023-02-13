acbb53f2fcd30b96922750082127feca78a1a9f7
Renanbfreitas <renanbf1992@outlook.com>
2023-02-13 17:28:02 -0300

    fix: req 1 e 2

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 3306,
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  user: process.env.MYSQL_ROOT || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
